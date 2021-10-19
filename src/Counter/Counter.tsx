24446cea83d933072b369a3e3565a09c5b5be2ae
Alexander <Alexander_alatorre1993@hotmail.com>
2021-10-19 16:53:58 -0600

    Added typescript

---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface CounterProps {
+  defaultCount: number;
+}
+
+interface CounterState {
+  count: number;
+}
+
+//you need to pass the props and state of the paramenters
+class Counter extends React.Component<CounterProps, CounterState> {
+  constructor(props: CounterProps) {
+    super(props);
+
+    this.state = {
+      count: props.defaultCount,
+    };
+  }
+  
+  state: CounterState;
+
+  increment = () => {
+    this.setState(({ count }) => ({
+      count: count - 1,
+    }));
+  };
+  decrement = () => {
+    this.setState(({ count }) => ({
+      count: count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={increment}>-</button>
+        <button onClick={decrement}>+</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Counter defaultCount={0} />, rootElement);
